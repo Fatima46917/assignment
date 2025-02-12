f76766d37c9938bd431de0b573a6efd7aa23b0de
Fatima46917 <fatumash44@gmail.com>
2025-02-12 22:28:49 +0200

    Add files via upload

---
+++ exersice1.py
@@ -0,0 +1,13 @@
+age = int(input("Please enter your age: "))
+if age >= 80:
+    print("The ticket price is 25$")
+elif age >= 60:
+    print("The ticket price is 20$")
+elif age >= 40:
+    print("The ticket price is 15$")
+elif age >= 20:
+    print("The ticket price is 10$")
+else:
+    print("The ticket price is 5$")
+
+ 
---
+++ exersice3.py
@@ -0,0 +1,8 @@
+username = str(input("Enter username: "))
+passward = input("Enter passward: ")
+
+if username == "admin" and passward == 1234:
+    print("Access Granted.")
+else:
+    print("Access Denied")
+    
