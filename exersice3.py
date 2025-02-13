02762f3f295e6e1a99298589dd44c08e7fefcece
fatima_sharafaldeen <fatumash44@gmail.com>
2025-02-13 15:28:42 +0200

    Update exersice3.py
    fix

--- exersice3.py
+++ exersice3.py
@@ -1,7 +1,7 @@
 username = str(input("Enter username: "))
-passward = input("Enter passward: ")
+passward = str(input("Enter passward: "))
 
-if username == "admin" and passward == 1234:
+if username == "admin" and passward == "1234":
     print("Access Granted.")
 else:
     print("Access Denied")
